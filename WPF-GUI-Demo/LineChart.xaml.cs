1a2f83005f954aba31572eb8d35bd8f927eb8b34
amim7925 <amim.464432@gmail.com>
2020-11-16 11:21:55 +0000

    first

---
+++ WPF-GUI-Demo/LineChart.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_GUI_Demo
+{
+    /// <summary>
+    /// Interaction logic for LineChart.xaml
+    /// </summary>
+    public partial class LineChart : UserControl
+    {
+        public LineChart()
+        {
+            InitializeComponent();
+            Values1 = new ChartValues<double> { 3, 4, 6, 3, 2, 6 };
+            Values2 = new ChartValues<double> { 5, 3, 5, 7, 3, 9 };
+
+            DataContext = this;
+        }
+
+        public ChartValues<double> Values1 { get; set; }
+        public ChartValues<double> Values2 { get; set; }
+    }
+}
---
+++ WPF-GUI-Demo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_GUI_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGoleft_Click(object sender, RoutedEventArgs e)
+        {
+            BtnGoleft.Visibility = Visibility.Hidden;
+            BtnGoright.Visibility = Visibility.Visible;
+        }
+
+        private void BtnGoright_Click(object sender, RoutedEventArgs e)
+        {
+            BtnGoright.Visibility = Visibility.Hidden;
+            BtnGoleft.Visibility = Visibility.Visible;
+        }
+
+        private void btnpower_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGoDown_Click(object sender, RoutedEventArgs e)
+        {
+            BtnGoDown.Visibility = Visibility.Hidden;
+            BtnGoTop.Visibility = Visibility.Visible;
+        }
+
+        private void BtnGoTop_Click(object sender, RoutedEventArgs e)
+        {
+            BtnGoTop.Visibility = Visibility.Hidden;
+            BtnGoDown.Visibility = Visibility.Visible;
+        }
+    }
+}
