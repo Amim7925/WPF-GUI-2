6910f0ce786879b7d918f9738253d3e4fca06e81
amim7925 <amim.464432@gmail.com>
2020-11-17 15:59:09 +0330

    excel file upload added

--- WPF-GUI-Demo/Forms/UploadCSV.xaml.cs
+++ WPF-GUI-Demo/Forms/UploadCSV.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +27,48 @@ namespace WPF_GUI_Demo.Forms
         public UploadCSV()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter =  "Excel Files|*.xls;*.xlsx;*.xlsm";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                path = openFileDialog.FileName;
+            }
+            if (path == string.Empty)
+                MessageBox.Show("your path is not valid");
+            DataTable dt = new DataTable();
+            using (XLWorkbook workbook = new XLWorkbook(path))
+            {
+                bool _isFirstRow = true;
+                var rows = workbook.Worksheet(1).RowsUsed();
+                foreach (var row in rows)
+                {
+                    if (_isFirstRow)
+                    {
+                        foreach (IXLCell cell in row.Cells())
+                        {
+                            dt.Columns.Add(cell.Value.ToString());
+
+                        }
+                        _isFirstRow = false;
+                    }
+                    else
+                    {
+                        dt.Rows.Add();
+                        int i = 0;
+                        foreach (IXLCell cell in row.Cells())
+                        {
+                            dt.Rows[dt.Rows.Count - 1][i++] = cell.Value.ToString();
+                        }
+                    }
+                }
+                //test datagrid to make sure data is being displayed as it should
+                datagrid_CsvFile.ItemsSource = dt.DefaultView;
+            }
         }
     }
 }
