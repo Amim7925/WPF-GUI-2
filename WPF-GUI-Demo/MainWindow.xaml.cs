63f16bd8436340523cd21c6b5930060daf2cf3c3
amim.7925 <amim.464432@gmail.com>
2020-11-17 11:24:52 +0330

    pop up winodw added

--- WPF-GUI-Demo/MainWindow.xaml.cs
+++ WPF-GUI-Demo/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace WPF_GUI_Demo
         {
             InitializeComponent();
         }
-
         private void BtnGoleft_Click(object sender, RoutedEventArgs e)
         {
             BtnGoleft.Visibility = Visibility.Hidden;
@@ -47,5 +46,56 @@ namespace WPF_GUI_Demo
 
         }
 
+        private void btnOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            btnOpenMenu.Visibility = Visibility.Hidden;
+            btnCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void btnCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            btnOpenMenu.Visibility = Visibility.Visible;
+            btnCloseMenu.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void btnTCPConnection_Click(object sender, RoutedEventArgs e)
+        {
+            new PopupWindow(4).ShowDialog();
+        }
+
+        private void btnUser_Click(object sender, RoutedEventArgs e)
+        {
+            new PopupWindow(3).ShowDialog();
+        }
+
+        private void btnCanbusConnection_Click(object sender, RoutedEventArgs e)
+        {
+            new PopupWindow(1).ShowDialog();
+        }
+
+        private void btnUploadCSVFile_Click(object sender, RoutedEventArgs e)
+        {
+            new PopupWindow(5).ShowDialog();
+        }
+
+        private void btnDrivingCycle_Click(object sender, RoutedEventArgs e)
+        {
+            new PopupWindow(2).ShowDialog();
+        }
+
+        private void btnAboutUs_Click(object sender, RoutedEventArgs e)
+        {
+            new PopupWindow(6).ShowDialog();
+        }
+
+        private void MainItemsGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
     }
 }
