f579691fddb5e1ed5e3d171486e85c400fe0fd99
amim7925 <amim.464432@gmail.com>
2020-11-21 17:33:54 +0000

    UC SimulationList

---
+++ WPF-GUI-Demo/SimulationList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_GUI_Demo
+{
+    /// <summary>
+    /// Interaction logic for SimulationList.xaml
+    /// </summary>
+    public partial class SimulationList : UserControl
+    {
+        string id;
+        public SimulationList(string _id)
+        {
+            InitializeComponent();
+            id = _id;
+        }
+
+
+
+
+
+
+
+    }
+}
